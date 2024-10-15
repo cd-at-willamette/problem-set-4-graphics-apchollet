4eec16f59ac1eb43d020ed300c792b176d1ada87
Alyssa Chollet <apchollet@willamette.edu>
2024-10-14 17:55:55 -0700

    Update Prob3.py

--- Prob3.py
+++ Prob3.py
@@ -15,8 +15,9 @@ SCORE_DY = 10                       # Distance up from bottom of window to basel
 SCORE_FONT = "bold 40pt 'serif'"    # Font for score
 a = GW_WIDTH//2 - SQUARE_SIZE//2
 b = GW_HEIGHT//2 - SQUARE_SIZE//2
-c = 0
-d = 0
+c = random.randint(0, 450)
+d = random.randint(0, 450)
+
 def clicky_box():
 
     # Defining the callback function, which you won't need until Part C
@@ -26,22 +27,23 @@ def clicky_box():
 
 
     def click_func(e): # compare x to c and d if x is in the range
-        c = random.randint(0, 450)
-        print('c=', c)
-        d = random.randint(0, 450)
-        print('d=', d)
+        global c
+        global d
         x = e.get_x()
         y = e.get_y()
         print('x,y =', x,y)
-        if c <= x >= (c+ SQUARE_SIZE) and d <= y >= (d + SQUARE_SIZE):
+        if c <= x <= (c+ SQUARE_SIZE) and d <= y <= (d + SQUARE_SIZE):
+            c = random.randint(0, 450)
+            d = random.randint(0, 450)
             gw.box.set_location(c, d)
-    gw.box = GRect(GW_WIDTH//2 - SQUARE_SIZE//2 + c, GW_HEIGHT//2 - SQUARE_SIZE//2 + d, SQUARE_SIZE, SQUARE_SIZE)
+            print(True)
+        else:
+            print(False)
+    gw.box = GRect(c , d, SQUARE_SIZE, SQUARE_SIZE)
     gw.box.set_filled(True)
     gw.box.set_color('pink')
     gw.add(gw.box)
-    # I need to get the edge cords of the box and then use <= and >= to compare x and y to those point to see if
-    # clicked in the box then move the box to a random place a and b
-
+    
 
 
 
